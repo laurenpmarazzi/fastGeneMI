a4598848150e7797751ff409517989e2f286155f
Jonathan Ish-Horowicz <jonathan_ish@hotmail.co.uk>
2018-12-20 16:46:49 +0000

    Adding MIT licene

--- src/bspline_mi.cpp
+++ src/bspline_mi.cpp
@@ -1,11 +1,3 @@
-// ----------------------------------------------------------------------------------
-//  Implementation of the B-spline Mutual information estimation method
-// ----------------------------------------------------------------------------------
-
-#include "fastGeneMI.h"
-
-using namespace Rcpp;
-
 // MIT License
 
 // Copyright (c) 2018 Jonathan Ish-Horowicz
@@ -27,6 +19,14 @@ using namespace Rcpp;
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+// ----------------------------------------------------------------------------------
+//  Implementation of the B-spline Mutual information estimation method
+// ----------------------------------------------------------------------------------
+
+#include "fastGeneMI.h"
+
+using namespace Rcpp;
 
 // ----------------------------------------------------------------------------------
 //  Class that returns bspline coefficients
